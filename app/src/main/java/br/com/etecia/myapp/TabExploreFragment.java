5f71cac30a6f0d2babf1360451442798ec4fe0ee
soalarii <soaresmachadolarissa@gmail.com>
2023-11-16 21:55:34 -0300

    Quarto commit

---
+++ app/src/main/java/br/com/etecia/myapp/TabExploreFragment.java
@@ -0,0 +1,17 @@
+package br.com.etecia.myapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TabExploreFragment extends Fragment {  @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab_explore, container, false);
+    }
+}
